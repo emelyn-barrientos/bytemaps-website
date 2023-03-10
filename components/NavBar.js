530faf287dfd311e0521c184bd169d53cf31f327
emelyn-barrientos <emelyn.barrientos22@gmail.com>
2023-03-10 17:10:19 +1300

    Add <nav> tag and import Link from React

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,5 +1,12 @@
 import React from 'react'
+import Link from 'next/link'
 
 export default function NavBar() {
-  return <h1>Bytemaps</h1>
+  return (
+    <nav className="navbar">
+      <Link href="/">
+        <h1 className="site-title">Bytemaps</h1>
+      </Link>
+    </nav>
+  )
 }
