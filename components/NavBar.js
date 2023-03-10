a80843b88cfe5cc752dc3a15b2fad9d7ae9bf05b
emelyn-barrientos <emelyn.barrientos22@gmail.com>
2023-03-10 17:19:27 +1300

    Wrap <nav> tag in fragment

--- components/NavBar.js
+++ components/NavBar.js
@@ -3,21 +3,23 @@ import Link from 'next/link'
 
 export default function NavBar() {
   return (
-    <nav className="nav-bar">
-      <Link href="/">
-        <h1 className="site-title">Bytemaps</h1>
-      </Link>
-      <ul className="nav-list">
+    <>
+      <nav className="nav-bar">
         <Link href="/">
-          <li className="navlist-item">Home</li>
+          <h1 className="site-title">Bytemaps</h1>
         </Link>
-        <Link href="https://www.instagram.com/bytemaps/">
-          <li className="navlist-item">Instagram</li>
-        </Link>
-        <Link href="/info">
-          <li className="navlist-item">Info</li>
-        </Link>
-      </ul>
-    </nav>
+        <ul className="nav-list">
+          <Link href="/">
+            <li className="navlist-item">Home</li>
+          </Link>
+          <Link href="https://www.instagram.com/bytemaps/">
+            <li className="navlist-item">Instagram</li>
+          </Link>
+          <Link href="/info">
+            <li className="navlist-item">Info</li>
+          </Link>
+        </ul>
+      </nav>
+    </>
   )
 }
