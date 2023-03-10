83d2e7815b8b5055e53df0594370a758d36a04e8
emelyn-barrientos <emelyn.barrientos22@gmail.com>
2023-03-10 17:13:47 +1300

    Add <ul> tag for nav bar list items

--- components/NavBar.js
+++ components/NavBar.js
@@ -3,10 +3,15 @@ import Link from 'next/link'
 
 export default function NavBar() {
   return (
-    <nav className="navbar">
+    <nav className="nav-bar">
       <Link href="/">
         <h1 className="site-title">Bytemaps</h1>
       </Link>
+      <ul className="nav-list">
+        <li className="navlist-item">Home</li>
+        <li className="navlist-item">Instagram</li>
+        <li className="navlist-item">Info</li>
+      </ul>
     </nav>
   )
 }
