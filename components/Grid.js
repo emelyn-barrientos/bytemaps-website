37ffbd87c725c3ead4a8bf3d3e591c925de3de84
emelyn-barrientos <emelyn.barrientos22@gmail.com>
2023-03-10 23:35:50 +1300

    Import Grid.module.scss into component and apply className syntax

--- components/Grid.js
+++ components/Grid.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import PostCard from './PostCard'
+import styles from '../styles/grid.module.scss'
 
 export default function Grid({ posts }) {
   return (
-    <div className="grid-container">
+    <div className={styles['grid-container']}>
       <PostCard posts={posts} />
     </div>
   )
