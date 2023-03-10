4acc3b70fb1a5605a29a4f3efe86ccffa55ea9bb
emelyn-barrientos <emelyn.barrientos22@gmail.com>
2023-03-10 22:45:34 +1300

    Remove uneeded imports and return statement

--- components/Grid.js
+++ components/Grid.js
@@ -1,39 +1,10 @@
 import React from 'react'
-import Image from 'next/image'
-import Link from 'next/link'
 import PostCard from './PostCard'
-import variables from '@/styles/variables.module.scss'
 
 export default function Grid({ posts }) {
-  const getImageUrlFromContent = (content) => {
-    const regex = /<img.*?src="(.*?)"/
-    const match = regex.exec(content)
-    return match ? match[1] : null
-  }
-
   return (
     <div>
-      <PostCard />
-      {posts &&
-        posts.map((post) => {
-          const imageUrl = getImageUrlFromContent(post.content)
-
-          return (
-            <div className={variables.title} key={post.id}>
-              <Link href={post.uri}>
-                <h1>{post.title}</h1>
-              </Link>
-              {imageUrl && (
-                <Image
-                  src={imageUrl}
-                  alt={post.title}
-                  width={500}
-                  height={500}
-                />
-              )}
-            </div>
-          )
-        })}
+      <PostCard posts={posts} />
     </div>
   )
 }
