b92f8b7914839cf16ac7c08612dff293d581deb9
emelyn-barrientos <emelyn.barrientos22@gmail.com>
2023-03-10 17:48:49 +1300

    Move posts.map() func into Grid.js component

--- components/Grid.js
+++ components/Grid.js
@@ -1,5 +1,24 @@
 import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Grid() {
-  return <h2>This is the Grid.</h2>
+  return (
+    <div>
+      {posts.map((post) => {
+        const imageUrl = getImageUrlFromContent(post.content)
+
+        return (
+          <div class={variables.title} key={post.id}>
+            <Link href={post.uri}>
+              <h1>{post.title}</h1>
+            </Link>
+            {imageUrl && (
+              <Image src={imageUrl} alt={post.title} width={500} height={500} />
+            )}
+          </div>
+        )
+      })}
+    </div>
+  )
 }
