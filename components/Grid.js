85ee4ecc32f94adaecb67dbc7523b90734bd6ac8
emelyn-barrientos <emelyn.barrientos22@gmail.com>
2023-03-10 18:03:36 +1300

    Import varaibles and remove getStatiProps func

--- components/Grid.js
+++ components/Grid.js
@@ -1,56 +1,38 @@
 import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
+import variables from '@/styles/variables.module.scss'
 
 export default function Grid({ posts }) {
+  console.log('posts: ', posts)
   const getImageUrlFromContent = (content) => {
     const regex = /<img.*?src="(.*?)"/
     const match = regex.exec(content)
     return match ? match[1] : null
   }
+
   return (
     <div>
-      {posts.map((post) => {
-        const imageUrl = getImageUrlFromContent(post.content)
+      {posts &&
+        posts.map((post) => {
+          const imageUrl = getImageUrlFromContent(post.content)
 
-        return (
-          <div class={variables.title} key={post.id}>
-            <Link href={post.uri}>
-              <h1>{post.title}</h1>
-            </Link>
-            {imageUrl && (
-              <Image src={imageUrl} alt={post.title} width={500} height={500} />
-            )}
-          </div>
-        )
-      })}
+          return (
+            <div className={variables.title} key={post.id}>
+              <Link href={post.uri}>
+                <h1>{post.title}</h1>
+              </Link>
+              {imageUrl && (
+                <Image
+                  src={imageUrl}
+                  alt={post.title}
+                  width={500}
+                  height={500}
+                />
+              )}
+            </div>
+          )
+        })}
     </div>
   )
-}
-
-export async function getStaticProps() {
-  const GET_ALL_POSTS = gql`
-    query GetAllPosts {
-      posts {
-        nodes {
-          title
-          id
-          date
-          uri
-          content
-        }
-      }
-    }
-  `
-  const res = await client.query({
-    query: GET_ALL_POSTS,
-  })
-
-  const posts = res?.data?.posts?.nodes
-
-  return {
-    props: {
-      posts,
-    },
-  }
 }
