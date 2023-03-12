c18571d66f53358690b4a1e3bbcff86d54677130
emelyn-barrientos <emelyn.barrientos22@gmail.com>
2023-03-12 18:04:35 +1300

    Remove posts.map from this component

--- components/PostCard.js
+++ components/PostCard.js
@@ -1,36 +1,23 @@
 import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
-import variables from '@/styles/Variables.module.scss'
 
-export default function PostCard({ posts }) {
+export default function PostCard({ post }) {
   const getImageUrlFromContent = (content) => {
     const regex = /<img.*?src="(.*?)"/
     const match = regex.exec(content)
     return match ? match[1] : null
   }
-  return (
-    <div>
-      {posts &&
-        posts.map((post) => {
-          const imageUrl = getImageUrlFromContent(post.content)
+  const imageUrl = getImageUrlFromContent(post.content)
 
-          return (
-            <div className={variables.title} key={post.id}>
-              {imageUrl && (
-                <Image
-                  src={imageUrl}
-                  alt={post.title}
-                  width={500}
-                  height={500}
-                />
-              )}
-              <Link href={post.uri}>
-                <h1>{post.title}</h1>
-              </Link>
-            </div>
-          )
-        })}
+  return (
+    <div key={post.id}>
+      {imageUrl && (
+        <Image src={imageUrl} alt={post.title} width={500} height={500} />
+      )}
+      <Link href={post.uri}>
+        <h1>{post.title}</h1>
+      </Link>
     </div>
   )
 }
