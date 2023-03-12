46727e6ee0732431057b1791b2ac2e75aa8b4ae7
emelyn-barrientos <emelyn.barrientos22@gmail.com>
2023-03-12 21:09:37 +1300

    Hardcode image width and height

--- components/PostCard.js
+++ components/PostCard.js
@@ -21,9 +21,9 @@ export default function PostCard({ post }) {
           <Image
             src={imageUrl}
             alt={post.title}
-            width={750}
-            height={750}
-            layout="responsive"
+            width={500}
+            height={500}
+            sizes="(min-width: 1024px) 33.33vw, (min-width: 768px) 50vw, 100vw"
           />
         </div>
       )}
