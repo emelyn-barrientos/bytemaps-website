409fe1f43e9cd8da41bea3a54ec5e9185a8a18ef
emelyn-barrientos <emelyn.barrientos22@gmail.com>
2023-03-12 21:22:14 +1300

    Removed PostCard.module.scss imports and className attributes

--- components/PostCard.js
+++ components/PostCard.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
-import styles from '../styles/PostCard.module.scss'
 
 export default function PostCard({ post }) {
   const getImageUrlFromContent = (content) => {
@@ -14,17 +13,8 @@ export default function PostCard({ post }) {
   return (
     <div key={post.id}>
       {imageUrl && (
-        <div
-          className={styles['image-container']}
-          style={{ objectFit: 'cover', objectPosition: 'center' }}
-        >
-          <Image
-            src={imageUrl}
-            alt={post.title}
-            width={500}
-            height={500}
-            sizes="(min-width: 1024px) 33.33vw, (min-width: 768px) 50vw, 100vw"
-          />
+        <div>
+          <Image src={imageUrl} alt={post.title} width={500} height={500} />
         </div>
       )}
       <Link href={post.uri}>
