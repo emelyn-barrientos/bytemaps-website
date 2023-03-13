20a4e0177ecefd517dd5b7d530e1daee5a9c65bd
emelyn-barrientos <emelyn.barrientos22@gmail.com>
2023-03-13 17:52:13 +1300

    Add Head and Nav into func

--- components/Header.js
+++ components/Header.js
@@ -2,5 +2,18 @@ import Head from 'next/head'
 import NavBar from '@/components/NavBar'
 
 export default function Header() {
-  return ()
+  return (
+    <header>
+      <Head>
+        <title>Bytemaps</title>
+        <meta
+          name="description"
+          content="Digital artwork gallery by Bytemaps."
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <NavBar />
+    </header>
+  )
 }
