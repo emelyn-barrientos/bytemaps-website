7b55944de0edba760b8e2fad144663c4dc9ae90e
emelyn-barrientos <emelyn.barrientos22@gmail.com>
2023-03-13 18:17:58 +1300

    Import Header component into _app.js to make Header universal across all pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import '../styles/globals.scss'
+import Header from '@/components/Header'
 import { ApolloProvider } from '@apollo/client/react'
 import { client } from '@/lib/apolloClient'
 
 export default function App({ Component, pageProps }) {
   return (
     <ApolloProvider client={client}>
+      <Header />
       <Component {...pageProps} />
     </ApolloProvider>
   )
