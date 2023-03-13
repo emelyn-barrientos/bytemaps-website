bf91bb130250fedf8ff77907674bda15916c9237
emelyn-barrientos <emelyn.barrientos22@gmail.com>
2023-03-13 19:56:21 +1300

    Import Head and NavBar to place with return statement and import title as pageProps

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,24 @@
+import Head from 'next/head'
+import NavBar from '@/components/NavBar'
 import '../styles/globals.scss'
-import Header from '@/components/Header'
 import { ApolloProvider } from '@apollo/client/react'
 import { client } from '@/lib/apolloClient'
 
 export default function App({ Component, pageProps }) {
+  const { title } = pageProps
+
   return (
     <ApolloProvider client={client}>
-      <Header />
+      <Head>
+        <meta
+          name="description"
+          content="Digital artwork gallery by Bytemaps."
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+        <title>{title ? `${title} - Bytemaps` : 'Bytemaps'}</title>
+      </Head>
+      <NavBar />
       <Component {...pageProps} />
     </ApolloProvider>
   )
