bce5a3afa85dc01e9f638a12ca6b08659a79bc5c
emelyn-barrientos <emelyn.barrientos22@gmail.com>
2023-03-14 21:34:46 +1300

    Import PreviousButton and NextButton

--- pages/[uri].js
+++ pages/[uri].js
@@ -1,4 +1,6 @@
 import Head from 'next/head'
+import PreviousButton from '@/components/PreviousButton'
+import NextButton from '@/components/NextButton'
 import styles from '../styles/Post.module.scss'
 import { client } from '../lib/apolloClient'
 import { gql } from '@apollo/client'
@@ -24,6 +26,8 @@ export default function SlugPage({ post }) {
             <source src={videoUrl} />
           </video>
         )}
+        <PreviousButton />
+        <NextButton />
       </div>
     </div>
   )
