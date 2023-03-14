af662685057482d36df1f96eef482256d485c5a0
emelyn-barrientos <emelyn.barrientos22@gmail.com>
2023-03-14 22:31:21 +1300

    Pass previousPostUri as prop into component

--- components/PreviousButton.js
+++ components/PreviousButton.js
@@ -1,11 +1,13 @@
 import Link from 'next/link'
 
-export default function PreviousButton({ postId }) {
+export default function PreviousButton({ previousPostUri }) {
   return (
     <div>
-      <Link href={`/posts/${postId}`}>
-        <h2>Previous</h2>
-      </Link>
+      {previousPostUri ? (
+        <Link href={`/posts/${previousPostUri}`}>
+          <h2>Previous</h2>
+        </Link>
+      ) : null}
     </div>
   )
 }
