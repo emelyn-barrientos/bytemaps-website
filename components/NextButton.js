5e621e7599b80a5d83fec44bd5455b7d410d92b9
emelyn-barrientos <emelyn.barrientos22@gmail.com>
2023-03-14 22:33:56 +1300

    Pass nextPostUri as prop into component

--- components/NextButton.js
+++ components/NextButton.js
@@ -1,11 +1,13 @@
 import Link from 'next/link'
 
-export default function NextButton({ postId }) {
+export default function NextButton({ nextPostUri }) {
   return (
     <div>
-      <Link href={`/posts/${postId}`}>
-        <h2>Next</h2>
-      </Link>
+      {nextPostUri ? (
+        <Link href={`/posts/${nextPostUri}`}>
+          <h2>Next</h2>
+        </Link>
+      ) : null}
     </div>
   )
 }
