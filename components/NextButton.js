50ed0907a0c8aba84dc2ac011056197cab8cd619
emelyn-barrientos <emelyn.barrientos22@gmail.com>
2023-03-15 21:59:29 +1300

    Import button class name into PreviousButton and NextButton components

--- components/NextButton.js
+++ components/NextButton.js
@@ -1,8 +1,9 @@
 import Link from 'next/link'
+import styles from '../styles/Buttons.module.scss'
 
 export default function NextButton({ nextPostUri }) {
   return (
-    <div>
+    <div className={styles['button']}>
       {nextPostUri ? (
         <Link href={`${nextPostUri}`}>
           <h2>Next</h2>
