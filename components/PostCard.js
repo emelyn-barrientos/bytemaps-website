1930e9699d261cc1a51ef7e266c0fc7e27bc2b00
emelyn-barrientos <emelyn.barrientos22@gmail.com>
2023-03-16 20:24:45 +1300

    Update Image props

--- components/PostCard.js
+++ components/PostCard.js
@@ -4,21 +4,18 @@ import Link from 'next/link'
 import styles from '../styles/PostCard.module.scss'
 
 export default function PostCard({ post }) {
-  const getImageUrlFromContent = (content) => {
-    const regex = /<img.*?src="(.*?)"/
-    const match = regex.exec(content)
-    return match ? match[1] : null
-  }
-  const imageUrl = getImageUrlFromContent(post.content)
-
+  console.log('post: ', post)
   return (
     <div key={post.id} className={styles['post-card']}>
       <Link href={post.uri}>
-        {imageUrl && (
-          <div className={styles['post-image']}>
-            <Image src={imageUrl} alt={post.title} width={500} height={500} />
-          </div>
-        )}
+        <div className={styles['post-image']}>
+          <Image
+            src={post.thumbnail.url}
+            alt={post.thumbnail.alt}
+            width={post.thumbnail.width}
+            height={post.thumbnail.height}
+          />
+        </div>
         <h2>{post.title}</h2>
       </Link>
     </div>
