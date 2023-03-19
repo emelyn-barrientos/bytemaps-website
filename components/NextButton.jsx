69d92e04df6168aa3fe10ad708e4fb0b782dd717
emelyn-barrientos <emelyn.barrientos22@gmail.com>
2023-03-19 22:31:48 +1300

    Uncomment out the button components

--- components/NextButton.jsx
+++ components/NextButton.jsx
@@ -1,10 +1,14 @@
-// import Link from 'next/link'
-// import styles from '../styles/Buttons.module.scss'
+import Link from 'next/link'
+import styles from '../styles/Buttons.module.scss'
 
-// export default function NextButton({ nextPostUri: uri }) {
-//   return (
-//     <Link href={`/${nextPostUri}`}>
-//       <h2 className={styles['button']}>Next</h2>
-//     </Link>
-//   )
-// }
+export default function NextButton({ nextPostUri }) {
+  return (
+    <div className={styles['button']}>
+      {nextPostUri ? (
+        <Link href={`${nextPostUri}`}>
+          <h2>Next</h2>
+        </Link>
+      ) : null}
+    </div>
+  )
+}
