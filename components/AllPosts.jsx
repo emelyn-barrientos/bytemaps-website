9388e3ef748137b14f50aa827a99015d2b106ac1
emelyn-barrientos <emelyn.barrientos22@gmail.com>
2023-04-04 14:29:41 +1200

    Wrap grid-container with motion.div

--- components/AllPosts.jsx
+++ components/AllPosts.jsx
@@ -5,13 +5,18 @@ import { motion, AnimatePresence } from 'framer-motion'
 export default function AllPosts({ posts }) {
   return (
     <AnimatePresence>
-      <div className={styles['grid-container']}>
+      <motion.div
+        className={styles['grid-container']}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+      >
         {posts.map((post) => (
           <div key={post.id} className={styles['grid-item']}>
             <PostCard post={post} />
           </div>
         ))}
-      </div>
+      </motion.div>
     </AnimatePresence>
   )
 }
