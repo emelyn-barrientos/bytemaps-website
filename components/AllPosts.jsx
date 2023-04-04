abc7f4ec1fea6e58bd85abb79864450d20057d58
emelyn-barrientos <emelyn.barrientos22@gmail.com>
2023-04-04 14:27:38 +1200

    Wrap component with AnimatePresence comp

--- components/AllPosts.jsx
+++ components/AllPosts.jsx
@@ -4,12 +4,14 @@ import { motion, AnimatePresence } from 'framer-motion'
 
 export default function AllPosts({ posts }) {
   return (
-    <div className={styles['grid-container']}>
-      {posts.map((post) => (
-        <div key={post.id} className={styles['grid-item']}>
-          <PostCard post={post} />
-        </div>
-      ))}
-    </div>
+    <AnimatePresence>
+      <div className={styles['grid-container']}>
+        {posts.map((post) => (
+          <div key={post.id} className={styles['grid-item']}>
+            <PostCard post={post} />
+          </div>
+        ))}
+      </div>
+    </AnimatePresence>
   )
 }
